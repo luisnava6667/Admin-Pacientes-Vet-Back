8c3c126ee10de1088f6028ddcffe5c8fe9a380f6
luisnava6667 <navaluisrodolfo@gmail.com>
2024-08-23 20:12:05 -0300

    first commit

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ dotenv.config()
 
 conectarDB()
 
-const dominiosPermitidos = [process.env.FRONTEND_URL]
+// const dominiosPermitidos = [process.env.FRONTEND_URL]
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (dominiosPermitidos.indexOf(origin) !== -1) {
-      // El Origen del Request esta permitido
-      callback(null, true)
-    } else {
-      callback(new Error('No permitido por CORS'))
-    }
-  }
-}
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (dominiosPermitidos.indexOf(origin) !== -1) {
+//       // El Origen del Request esta permitido
+//       callback(null, true)
+//     } else {
+//       callback(new Error('No permitido por CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use('/api/veterinarios', veterinarioRoutes)
 app.use('/api/pacientes', pacienteRoutes)
